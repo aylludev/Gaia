38f79dbb5e93a25682e15ad46e007e1a8864a402
aylludev <cafo1995@hotmail.com>
2025-05-02 16:09:34 -0500

    db

---
+++ Gaia/db.py
@@ -0,0 +1,23 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SQLITE = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# psycopg2
+
+POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ams',
+        'USER': 'postgres',
+        'PASSWORD': 'Apolo39',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
--- Gaia/settings.py
+++ Gaia/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import db
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,12 +82,7 @@ WSGI_APPLICATION = 'Gaia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = db.POSTGRESQL
 
 
 # Password validation
