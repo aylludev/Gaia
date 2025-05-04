5eb07cc5be7beb19feb080dae1d081f971bd082e
aylludev <cafo1995@hotmail.com>
2025-05-04 02:17:09 -0500

    dashboard 14%

--- Gaia/settings.py
+++ Gaia/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4)6oxk!ptf90741=m6t*qtf-&lbxwnrm1+=vrjk+chx4wtg#a='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['138.197.36.105']
 
 
 # Application definition
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'Gaia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = db.SQLITE
+DATABASES = db.POSTGRESQL
 
 
 # Password validation
